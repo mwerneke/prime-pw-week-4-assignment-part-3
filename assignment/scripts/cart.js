b935ed3fe1ea946280d9a82721bf7446be3993a9
Michael Werneke <mike.werneke@gmail.com>
2021-06-29 15:41:27 -0500

    week4_part3

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,25 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+
+
+
+let basket =[];
+  console.log( basket );
+
+function addItem(item){
+    console.log( 'Item added to Basket:', item);
+    basket.push( item );
+    return basket;
+
+} // end addItem
+function itemAdded(){
+  if(basket.length>0){
+    return true
+}
+  else{
+    return false
+    }
+}
+  addItem('Steak')
+  console.log('Was an item added to basket?',itemAdded());
